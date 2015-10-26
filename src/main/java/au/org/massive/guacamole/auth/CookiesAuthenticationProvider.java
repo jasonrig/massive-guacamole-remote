1ecc6ac0c1c783a50c2e8133e989af96564b72e2
jason <Jason.Rigby@monash.edu>
2015-10-26 16:32:50 +1100

    removed debugging statements

--- src/main/java/au/org/massive/guacamole/auth/CookiesAuthenticationProvider.java
+++ src/main/java/au/org/massive/guacamole/auth/CookiesAuthenticationProvider.java
@@ -1,15 +1,16 @@
 package au.org.massive.guacamole.auth;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import org.glyptodon.guacamole.GuacamoleException;
 import org.glyptodon.guacamole.net.auth.Credentials;
 import org.glyptodon.guacamole.net.auth.simple.SimpleAuthenticationProvider;
 import org.glyptodon.guacamole.protocol.GuacamoleConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,11 +18,6 @@ import java.util.Map;
  * Created by simonyu on 5/03/15.
  */
 public class CookiesAuthenticationProvider extends SimpleAuthenticationProvider {
-
-    /**
-     * Logger for this class.
-     */
-    private Logger logger = LoggerFactory.getLogger(CookiesAuthenticationProvider.class);
 
     @Override
     public Map<String, GuacamoleConfiguration> getAuthorizedConfigurations(Credentials credentials) throws GuacamoleException {
@@ -36,13 +32,21 @@ public class CookiesAuthenticationProvider extends SimpleAuthenticationProvider 
         for (Cookie c : request.getCookies()) {
             if (c.getName().startsWith("vnc-credentials")) {
                 //noinspection unchecked
-                vncCredentials = gson.fromJson(c.getValue(), Map.class);
+                try {
+                    //noinspection unchecked
+                    vncCredentials = gson.fromJson(URLDecoder.decode(c.getValue(), "utf-8"), Map.class);
 
-                configuration.setParameter("hostname", vncCredentials.get("hostname"));
-                configuration.setParameter("port", vncCredentials.get("port"));
-                configuration.setParameter("password", vncCredentials.get("password"));
-                configuration.setProtocol(vncCredentials.get("protocol"));
-                configurations.put(vncCredentials.get("name"), configuration);
+                    configuration.setParameter("hostname", vncCredentials.get("hostname"));
+                    configuration.setParameter("port", vncCredentials.get("port"));
+                    configuration.setParameter("password", vncCredentials.get("password"));
+                    configuration.setProtocol(vncCredentials.get("protocol"));
+                    configurations.put(vncCredentials.get("name"), configuration);
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (JsonSyntaxException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
 
